d22b12c6a007cfcaf69b9b2e84eef961f8bb7ba2
Ik <asemka-2012@mail.ru>
2018-08-15 15:53:18 -0400

    added line

--- src/main/GEclass.java
+++ src/main/GEclass.java
@@ -3,5 +3,16 @@ package main;
 public class GEclass {
 public static void main(String[] args) {
 	System.out.println("old project");
+	System.out.println("todai is a perfect day");
+	
+	int i = 79;
+	int j = 8849;
+	System.out.println(i+j);
+	
+	System.out.println("===============");
+	
+	for (int j2 = 0; j2 <= 10; j2++) {
+		System.out.println(j2);
+	}
 }
 }
